2a470578fe20a1ef917bc71fdf6c15094849c6d9
Miguel Cano <129132845+MiguelCano-ia@users.noreply.github.com>
2025-01-21 21:39:56 -0500

    Api updated

--- src/calendar/index.ts
+++ src/calendar/index.ts
@@ -1,6 +1,6 @@
+export * from "./pages/CalendarPage";
 export * from "./components/CalendarEvent.tsx";
 export * from "./components/CalendarModal.tsx";
 export * from "./components/FabAddNew.tsx";
 export * from "./components/FabDelete.tsx";
 export * from "./components/Navbar.tsx";
-export * from "./pages/CalendarPage";
